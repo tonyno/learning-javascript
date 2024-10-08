726892b600a8cd5ae296d48fb59b371705f761bb
Tonda Kmoch <tonda.kmoch@gmail.com>
2024-10-08 20:35:03 +0200

    task 2

---
+++ day002/day002.js
@@ -0,0 +1,23 @@
+// create function loginUser with two parameters: email and password
+// inside function create these constants:
+// - user1email: tonda.kmoch@gmail.com
+// - user1password: auto
+// - user2email: lenatko@gmail.com
+// - user2password: bubak
+// the function will return true, if email and password matches one of those users
+
+// (create function here)
+
+
+// create function test with two parameters: email and password
+// this function will print on screen if the email and password passes in loginUser function
+
+// (create function here)
+
+// write code that tests 4 different users with these combinations:
+// 1. good email and password
+// 2. good email but bad password
+// 3. bad email but good password
+// 4. different email and different password
+
+// (place the code here)
