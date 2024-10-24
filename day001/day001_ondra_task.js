9fbccceffcc699e4778d847428ae44184b3ccb26
Tonda Kmoch <tonda.kmoch@gmail.com>
2024-10-24 22:48:45 +0200

    ondrovy vytvory

---
+++ day001/day001_ondra_task.js
@@ -0,0 +1,18 @@
+// TASK:
+// getAge("Tonik") -> 14
+
+function getAge(name) {
+  if (name == "Tonik") return 14;
+  if (name == "Ondra") return 13;
+  if (name == "Tonda") return 43;
+  if (name == "Lenka") return 44;
+  if (name == "mata") return 10;
+  if (name == "Majda") return 8;
+}
+
+console.log("Tonik vek:", getAge("Tonik"));
+console.log("Ondra vek:", getAge("Ondra"));
+console.log("Tonda vek:", getAge("Tonda"));
+console.log("Lenka vek:", getAge("Lenka"));
+console.log("Mata vek:", getAge("mata"));
+console.log("Majda vek:", getAge("Majda"));
